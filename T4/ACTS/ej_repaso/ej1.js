b891bb090fb30e32bb46ca232c43da3acffc5398
Arturo Barba <arturo.barba@iesdonana.org>
2018-11-04 19:22:44 +0100

    Tareas

--- T4/ACTS/ej_repaso/ej1.js
+++ T4/ACTS/ej_repaso/ej1.js
@@ -1,6 +1,61 @@
-function modificar_valores(){
-    for (var i = 0; i < 3; i++) {
-        window.document.forms[0].element[i].values=Math.round(Math.ramdon()*99);
-    }
+var aciertos=0;
 
+function generarNumero(valor){
+    return Math.round(Math.random()*valor);
+}
+
+function generarFecha(valor){
+    do {
+        var fecha=Math.round(Math.random()*valor)+1970;
+    } while (fecha>valor);
+    return fecha;
+}
+
+function imprimirNumeros(){
+    for (var i = 1; i < 4; i++) {
+        document.getElementById(`n${i}`).value=generarNumero(99);
+    }
+}
+
+function imprimirFecha(){
+    var d = new Date().getFullYear();
+    document.getElementById(`fecha`).value=generarFecha(d);
+}
+
+var d = 60;
+var tiempo = setInterval(function(){
+    d--;
+    document.getElementById(`tiempo`).value=`${d}`;
+    if (d==0){
+        var my = window.open("", "my", "width=200, height=100");
+        my.document.write(`LA BOMBA EXPLOTO`);
+        clearInterval(tiempo)
+    }}
+, 1000);
+
+function comprobarNumeros(){
+    var a=[];
+    for (var i = 1; i < 4; i++) {
+        a.push(document.getElementById(`n${i}`).value);
+    }
+    return Math.max(...a);
+}
+
+function comprobarFecha(){
+    return document.getElementById(`tiempo`).value*8760;
+}
+
+function fin(){
+
+    if (document.getElementById(`p1`).value==comprobarNumeros()){
+        aciertos++;
+    }
+    if (document.getElementById(`p2`).value==comprobarFecha()){
+        aciertos++;
+    }
+    var tiempo = 60-d;
+    var my = window.open("", "my", "width=200, height=100");
+    my.document.write(`Has tardado ${tiempo} segundos. <br>
+        El numero de respuestas acertadas ha sido: ${aciertos}.`);
+    clearInterval(tiempo);
 }
