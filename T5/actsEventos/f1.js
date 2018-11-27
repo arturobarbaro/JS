76cf14cd1e03aa7c83685bd7e2436a9eeb85115d
Arturo Barba <arturo.barba@iesdonana.org>
2018-11-27 17:17:22 +0100

    Practicas

--- T5/actsEventos/f1.js
+++ T5/actsEventos/f1.js
@@ -1,4 +1,6 @@
+//rehacer
 function comprobar(){
+    //limpiar el campo
     console.clear();
     comprobarDNI();
     comprobarNombre();
@@ -8,16 +10,17 @@ function comprobar(){
     comprobarContraseña();
 }
 function comprobarDNI(){
-    var exp=/^\d{2}\.\d{3}.\d{3}-[TRWAGMYFPDXBNJZSQVHLCKE]$/gi
+    var exp=/^\d{2}\.\d{3}\.\d{3}\-[TRWAGMYFPDXBNJZSQVHLCKE]$/gi
     var e=document.getElementById('dni').value;
     if (e==''){
         console.log("Error: introduzca un DNI no vacío");
     }else if (!e.match(exp)){
         console.log("Error: formato de DNI no válido");
+        document.getElementById('dni').value='';
     }
 }
 function comprobarNombre(){
-    var exp=/^[A-ZÑÁÉIOU]+\s[A-ZÑÁÉIOU]+($||(\s[A-ZÑÁÉIOU]+$||(\s[A-ZÑÁÉIOU]+)?)?)/gi
+    var exp=/^[A-ZÑÁÉIOU]+\s[A-ZÑÁÉIOU]+($||(\s[A-ZÑÁÉIOU]+$||(\s[A-ZÑÁÉIOU]+$)?)?)/i
     var e=document.getElementById('nombre').value;
     if (e==''){
         console.log("Error: introduzca un nombre no vacío");
@@ -26,7 +29,7 @@ function comprobarNombre(){
     }
 }
 function comprobarNac(){
-    var exp=/^([012]\d||3[01])\/([0]\d||1[012])\/([1]\d{3}||20[01][0-8])$/gi
+    var exp=/^([012]\d|3[01])\/([0]\d|1[012])\/(1\d{3}|20[01][0-8])$/i
     var e=document.getElementById('nac').value;
     if (e==''){
         console.log("Error: introduzca una fecha");
@@ -35,7 +38,7 @@ function comprobarNac(){
     }
 }
 function comprobarEmail(){
-    var exp=/^\w+@\w+\.[a-z]+$/gi
+    var exp=/^\w+@\w+\.[a-z]{2,3}$/i
     var e=document.getElementById('email').value;
     if (e==''){
         console.log("Error: introduzca una dirección de email");
@@ -44,7 +47,7 @@ function comprobarEmail(){
     }
 }
 function comprobarWeb(){
-    var exp=/^http:\/\/[a-z0-9]+\.[a-z]+$/gi
+    var exp=/^http[s]?:\/\/[a-z0-9]+\.[a-z]{2,3}$/i
     var e=document.getElementById('web').value;
     if (e==''){
         console.log("Error: introduzca una web");
@@ -53,7 +56,7 @@ function comprobarWeb(){
     }
 }
 function comprobarContraseña(){
-    var exp=/^\w{8,10}$/gi
+    var exp=/^\w{8,10}$/i
     var contraseña1=document.getElementById('c1').value;
     var contraseña2=document.getElementById('c2').value;
     if (contraseña1=='' || contraseña2==''){
