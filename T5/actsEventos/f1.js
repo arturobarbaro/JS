eb3b157a485b403bd60d69926c89089747302112
Arturo Barba <arturo.barba@iesdonana.org>
2018-11-28 12:11:04 +0100

    Correcciones

--- T5/actsEventos/f1.js
+++ T5/actsEventos/f1.js
@@ -2,58 +2,28 @@
 function comprobar(){
     //limpiar el campo
     console.clear();
-    comprobarDNI();
-    comprobarNombre();
-    comprobarNac();
-    comprobarEmail();
-    comprobarWeb();
+    comprobarCampo();
+    // comprobarDNI();
+    // comprobarNombre();
+    // comprobarNac();
+    // comprobarEmail();
+    // comprobarWeb();
     comprobarContraseña();
 }
-function comprobarDNI(){
-    var exp=/^\d{2}\.\d{3}\.\d{3}\-[TRWAGMYFPDXBNJZSQVHLCKE]$/gi
-    var e=document.getElementById('dni').value;
-    if (e==''){
-        console.log("Error: introduzca un DNI no vacío");
-    }else if (!e.match(exp)){
-        console.log("Error: formato de DNI no válido");
-        document.getElementById('dni').value='';
-    }
-}
-function comprobarNombre(){
-    var exp=/^[A-ZÑÁÉIOU]+\s[A-ZÑÁÉIOU]+($||(\s[A-ZÑÁÉIOU]+$||(\s[A-ZÑÁÉIOU]+$)?)?)/i
-    var e=document.getElementById('nombre').value;
-    if (e==''){
-        console.log("Error: introduzca un nombre no vacío");
-    }else if (!e.match(exp)){
-        console.log("Error: formato de nombre no válido");
-    }
-}
-function comprobarNac(){
-    var exp=/^([012]\d|3[01])\/([0]\d|1[012])\/(1\d{3}|20[01][0-8])$/i
-    var e=document.getElementById('nac').value;
-    if (e==''){
-        console.log("Error: introduzca una fecha");
-    }else if (!e.match(exp)){
-        console.log("Error: formato de fecha de nacimiento no válido");
-    }
-}
-function comprobarEmail(){
-    var exp=/^\w+@\w+\.[a-z]{2,3}$/i
-    var e=document.getElementById('email').value;
-    if (e==''){
-        console.log("Error: introduzca una dirección de email");
-    }else if (!e.match(exp)){
-        console.log("Error: formato de email no válido");
-    }
-}
-function comprobarWeb(){
-    var exp=/^http[s]?:\/\/[a-z0-9]+\.[a-z]{2,3}$/i
-    var e=document.getElementById('web').value;
-    if (e==''){
-        console.log("Error: introduzca una web");
-    }else if (!e.match(exp)){
-        console.log("Error: formato de página web no válido");
-    }
+function comprobarCampo(){
+    var campos=document.getElementsByTagName('input');
+    var exps=[/^\d{2}\.\d{3}\.\d{3}\-[TRWAGMYFPDXBNJZSQVHLCKE]$/gi,/^[A-ZÑÁÉIOU]+\s[A-ZÑÁÉIOU]+($||(\s[A-ZÑÁÉIOU]+$||(\s[A-ZÑÁÉIOU]+$)?)?)/i,
+        /^([012]\d|3[01])\/([0]\d|1[012])\/(1\d{3}|20[01][0-8])$/i,/^\w+@\w+\.[a-z]{2,3}$/i,/^http[s]?:\/\/[a-z0-9]+\.[a-z]{2,3}$/i];
+        for (var i = 0; i < exps.length; i++) {
+            var e=campos[i].value;
+            var exp=exps[i];
+            if (e==''){
+                console.log(`Error: introduzca un ${campos[i].name} no vacío`);
+            }else if (!e.match(exp)){
+                console.log(`Error: formato de ${campos[i].name} no válido`);
+                campos[i].value='';
+            }
+        }
 }
 function comprobarContraseña(){
     var exp=/^\w{8,10}$/i
@@ -70,3 +40,50 @@ function comprobarContraseña(){
 window.onload = function(){
     document.getElementById('b1').addEventListener('click',comprobar);
 }
+
+// function comprobarDNI(){
+//     var exp=/^\d{2}\.\d{3}\.\d{3}\-[TRWAGMYFPDXBNJZSQVHLCKE]$/gi
+//     var e=document.getElementById('dni').value;
+//     if (e==''){
+//         console.log("Error: introduzca un DNI no vacío");
+//     }else if (!e.match(exp)){
+//         console.log("Error: formato de DNI no válido");
+//         document.getElementById('dni').value='';
+//     }
+// }
+// function comprobarNombre(){
+//     var exp=/^[A-ZÑÁÉIOU]+\s[A-ZÑÁÉIOU]+($||(\s[A-ZÑÁÉIOU]+$||(\s[A-ZÑÁÉIOU]+$)?)?)/i
+//     var e=document.getElementById('nombre').value;
+//     if (e==''){
+//         console.log("Error: introduzca un nombre no vacío");
+//     }else if (!e.match(exp)){
+//         console.log("Error: formato de nombre no válido");
+//     }
+// }
+// function comprobarNac(){
+//     var exp=/^([012]\d|3[01])\/([0]\d|1[012])\/(1\d{3}|20[01][0-8])$/i
+//     var e=document.getElementById('nac').value;
+//     if (e==''){
+//         console.log("Error: introduzca una fecha");
+//     }else if (!e.match(exp)){
+//         console.log("Error: formato de fecha de nacimiento no válido");
+//     }
+// }
+// function comprobarEmail(){
+//     var exp=/^\w+@\w+\.[a-z]{2,3}$/i
+//     var e=document.getElementById('email').value;
+//     if (e==''){
+//         console.log("Error: introduzca una dirección de email");
+//     }else if (!e.match(exp)){
+//         console.log("Error: formato de email no válido");
+//     }
+// }
+// function comprobarWeb(){
+//     var exp=/^http[s]?:\/\/[a-z0-9]+\.[a-z]{2,3}$/i
+//     var e=document.getElementById('web').value;
+//     if (e==''){
+//         console.log("Error: introduzca una web");
+//     }else if (!e.match(exp)){
+//         console.log("Error: formato de página web no válido");
+//     }
+// }
