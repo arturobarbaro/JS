1a996dee02ef7ab80a7cb5faaa52974233c13114
Arturo Barba <arturo.barba@iesdonana.org>
2018-11-29 23:20:22 +0100

    Tareas validas

--- T5/cookies/act3.js
+++ T5/cookies/act3.js
@@ -1,15 +1,71 @@
-window.onload=function(){
-    function validacionNombre(){
-    }
+// function validacionHijos(e){
+//     e.preventDefault();
+//     if(document.getElementById('hijos').validity.valueMissing)){
+//         document.getElementById('hijos').setCustomValidity("Inserte un valor");
+//     }else if(document.getElementById('hijos').validity.rangeOverflow){
+//         document.getElementById('hijos').setCustomValidity("El numero de hijos debe ser maximo 10");
+//     }else if(document.getElementById('hijos').validity.rangeUnderflow){
+//         document.getElementById('hijos').setCustomValidity("El numero de hijos debe ser minimo 0");
+//     }
+// }
+//
+// // function validacionNombre(e){
+// //     e.preventDefault();
+// //     if (nombre.validity.patternMismatch) {
+// //         nombre.setCustomValidity('Formato erroneo);
+// //         //nombre.validationMessage = "Solo puede introducir caracteres alfabéticos de a - z"; //no se puede hacer, SOLO LECTURA
+// //     }
+// //     if (nombre.validity.valueMissing) {
+// //         nombre.setCustomValidity('Es obligatorio rellenar este campo del formulario');
+// //     }
+// //     error.innerHTML = nombre.validationMessage;
+// // }
+//
+// window.onload=function(){
+//
+//
+//
+//     //nombre.addEventListener('change',validacionNombre);
+//     hijos.addEventListener('change',validacionHijos);
+//     b1.addEventListener('change',function(){
+//         validacionNombre();
+//         validacionHijos();
+//     });
+// }
+function validarNombre(){
+    if (this.checkValidity() == false) {
+        document.getElementById('nombre').setCustomValidity("Inserte un valor");
+        if (nombre.validity.valueMissing) {
+            nombre.setCustomValidity('Es obligatorio rellenar este campo del formulario');
+        }else if (nombre.validity.patternMismatch) {
+            nombre.setCustomValidity('Formato erroneo');
+            //nombre.validationMessage = "Solo puede introducir caracteres alfabéticos de a - z"; //no se puede hacer, SOLO LECTURA
+        }
 
-    function validacionHijos(){
-        if(document.getElementById('hijos').validity.valueMissing)){
-            document.getElementById('hijos').setCustomValidity("Inserte un valor");
-        }else if(document.getElementById('hijos').validity.rangeOverflow){
+        document.getElementById("error").innerHTML =` ${this.validationMessage}. ${this.title}`;
+        this.value = '';
+    }
+}
+
+function validarHijos(){
+    if (this.checkValidity() == false) {
+        document.getElementById('hijos').setCustomValidity("Inserte un valor");
+        if(document.getElementById('hijos').validity.rangeOverflow){
             document.getElementById('hijos').setCustomValidity("El numero de hijos debe ser maximo 10");
         }else if(document.getElementById('hijos').validity.rangeUnderflow){
             document.getElementById('hijos').setCustomValidity("El numero de hijos debe ser minimo 0");
         }
+        document.getElementById("error").innerHTML =` ${this.validationMessage}`;
+        this.value = '';
     }
-    b1.addEventListener('click',validacionHijos);
+}
+
+window.onload = function(){
+  hijos.addEventListener('change',validarHijos)
+  nombre.addEventListener('change',validarNombre)
+
+  b1.onclick = function(e){
+    e.preventDefault(); //evitamos que salga el mensaje con el error
+  }
+
 }
