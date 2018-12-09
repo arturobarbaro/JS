65f5909e220e66eb43ad9660d7301b1903ec99d6
Arturo Barba <arturo.barba@iesdonana.org>
2018-12-09 12:46:25 +0100

    Tareas optimizadas

--- T6/f1.js
+++ T6/f1.js
@@ -15,24 +15,22 @@ function error(error, id){
     document.getElementById(id).appendChild(respuesta);
 }
 function comprobarContraseña(){
-    var exp=/^\w{8,10}$/i
-    var divs=document.getElementsByTagName('div');
+    var exp=/^\w{8,10}$/i;
     var contraseña1=document.getElementById('c1').value;
     var contraseña2=document.getElementById('c2').value;
-    eliminarP(divs[divs.length-2].id);
+    eliminarP("div6");
     if (contraseña1=='' || contraseña2==''){
-        error("Error: introduzca una contraseña no vacia",divs[divs.length-2].id);
+        error("Error: introduzca una contraseña no vacia","div6");
     }else if (contraseña1!=contraseña2) {
-        error("Error: las contraseñas no coinciden",divs[divs.length-2].id);
+        error("Error: las contraseñas no coinciden","div6");
     }else if (!contraseña1.match(exp)){
-        error("Error: formato de contraseña no válido",divs[divs.length-2].id);
+        error("Error: formato de contraseña no válido","div6");
     }
 }
-
 function eliminarP(id){
     var pa = document.getElementById(id).getElementsByTagName('p');
     for (var i = 0; i < pa.length; i++) {
-        document.getElementById(id).removeChild(pa[i]);
+        document.getElementById(id).removeChild(pa[0]);
     }
 }
 window.onload = function(){
@@ -44,29 +42,17 @@ window.onload = function(){
                 /^\w+@\w+\.[a-z]{2,3}$/i,
                 /^https?:\/\/www.[a-z0-9]+\.[a-z]{2,3}$/i
             ];
-    campos[0].addEventListener('change',function(){
-            comprobar(this,exps[0])
-    })
-    campos[1].addEventListener('change',function(){
-            comprobar(this,exps[1])
-    })
-    campos[2].addEventListener('change',function(){
-            comprobar(this,exps[2])
-    })
-    campos[3].addEventListener('change',function(){
-            comprobar(this,exps[3])
-    })
-    campos[4].addEventListener('change',function(){
-            comprobar(this,exps[4])
-    })
-
-    // FALLA AL PASAR exp[i]
-
+    // FALLA AL PASAR exp[i]. Por lo que asignamos los eventos manualmente.
     // for (var i = 0; i < campos.length -2; i++) {
     //     var exp= exps[i];
     //     campos[i].addEventListener('change',function(){
     //         comprobar(this,exp)
     //     })
     // }
+    campos[0].addEventListener('change',function(){comprobar(this,exps[0])})
+    campos[1].addEventListener('change',function(){comprobar(this,exps[1])})
+    campos[2].addEventListener('change',function(){comprobar(this,exps[2])})
+    campos[3].addEventListener('change',function(){comprobar(this,exps[3])})
+    campos[4].addEventListener('change',function(){comprobar(this,exps[4])})
     document.getElementById('b1').addEventListener('click',comprobarContraseña);
 }
