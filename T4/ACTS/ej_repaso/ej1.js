5816aa824c94c555fe60542aa2e4520dedc9a42a
Arturo Barba <arturo.barba@iesdonana.org>
2018-12-14 10:22:27 +0100

    Optimizaciones de ejercicios

--- T4/ACTS/ej_repaso/ej1.js
+++ T4/ACTS/ej_repaso/ej1.js
@@ -1,4 +1,12 @@
-
+var tiempo2 = 10;
+var d = 10;
+var tiempo = setInterval(function(){
+    d--;
+    document.getElementById(`tiempo`).value=`${d}`;
+    if (d==0){
+        abrirVentana('LA BOMBA EXPLOTÓ');
+    }}
+, 1000);
 
 function generarNumero(valor){
     return Math.round(Math.random()*valor);
@@ -22,20 +30,6 @@ function imprimirFecha(){
     document.getElementById(`fecha`).value=generarFecha(d);
 }
 
-var tiempo2 = 10;
-var d = 10;
-var tiempo = setInterval(function(){
-    d--;
-    document.getElementById(`tiempo`).value=`${d}`;
-    if (d==0){
-        //usar moveTo()
-        //funcion
-        var my = window.open("", "my", "width=200, height=100");
-        my.document.write(`LA BOMBA EXPLOTO`);
-        clearInterval(tiempo);
-    }}
-, 1000);
-
 function comprobarNumeros(){
     var a=[];
     for (var i = 1; i < 4; i++) {
@@ -56,12 +50,21 @@ function fin(){
     if (document.getElementById(`p2`).value==comprobarFecha()){
         aciertos++;
     }
+    b.removeEventListener('click',fin);
     var t = tiempo2-d;
-    clearInterval(tiempo);
-    var my = window.open("", "my", "width=200, height=100");
-    //llamada a funcion mens(parametro)
-    // comprobar()
-    my.document.write(`Has tardado ${t} segundos. <br>
+    abrirVentana(`Has tardado ${t} segundos. <br>
         El numero de respuestas acertadas ha sido: ${aciertos}.`);
 
+}
+
+function abrirVentana(mensaje){
+    var my = window.open("", "my", "width=200, height=100");
+    my.document.write(mensaje);
+    clearInterval(tiempo);
+}
+
+window.onload = function(){
+    imprimirFecha();
+    imprimirNumeros();
+    b.addEventListener('click',fin);
 }
