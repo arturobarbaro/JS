2c3878dcb26e2041fbb25e07312a4f6fc0c2a08f
Arturo Barba <arturo.barba@iesdonana.org>
2018-12-17 22:43:58 +0100

    Terminamos metodos

--- examen/EJERCICIO 1/ej1.js
+++ examen/EJERCICIO 1/ej1.js
@@ -95,7 +95,8 @@ function guardar(){
 
 window.onload = function(){
     var partida = 0;
-    getCookie('Partida')?partida=getCookie('Partida'):setCookie('Partida',partida++);
+    getCookie('Partida')?partida=getCookie('Partida'):'';
+    setCookie('Partida',partida++)
     var botones = Array.from(document.getElementsByTagName('button'));
     insertarTabla(botones[1]);
     botones[0].addEventListener('click',ventana);
